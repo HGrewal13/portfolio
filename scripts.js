ea3a52f58c426138c7d0591ce0fd0f3d865ec043
Harry <harrysgrewal13@gmail.com>
2019-06-19 10:19:11 -0400

    demo night copy

--- scripts.js
+++ scripts.js
@@ -2,27 +2,58 @@ websiteApp = {};
 
 websiteApp.focusedPiece = function() {
     if ($(`.hangmanItem`).on(`click`, function (e) {
-        $(`.focusedPieceText h4, .focusedPieceText p, .laptopScreen img`).addClass(`invisible`);
+        $(`.focusedPieceText h4,
+        .focusedPieceText h5, 
+        .focusedPieceText p, 
+        .liveButtons,
+        .laptopScreen img`).addClass(`invisible`);
         $(`.reportCardDay`).removeClass(`invisible`);
-        $(`.reportCardDay`).addClass(`visible`);
+        $(`.item`).removeClass(`itemFocus`);
+        $(this).addClass(`itemFocus`);
+        $(`h4.reportCardDay, p.reportCardDay`).addClass(`visible`);
+        $(`button.reportCardDay`).addClass(`visibleButton`);
+        // $(`.reportCardDay`).toggleClass(`invisible`);
+
     }));
 
     if ($(`.pokemonItem`).on(`click`, function (e) {
-        $(`.focusedPieceText h4, .focusedPieceText p, .laptopScreen img`).addClass(`invisible`);
+        $(`.focusedPieceText h4,
+        .focusedPieceText h5, 
+        .focusedPieceText p,
+        .liveButtons, 
+        .laptopScreen img`).addClass(`invisible`);
+        // $(`.showSkills`).addClass(`.invisible`);
         $(`.pokemonGame`).removeClass(`invisible`);
-        $(`.pokemonGame`).addClass(`visible`);
+        $(`.item`).removeClass(`itemFocus`);
+        $(this).addClass(`itemFocus`);
+        $(`h4.pokemonGame, p.reportCardDay`).addClass(`visible`);
+        $(`button.pokemonGame`).addClass(`visibleButton`);
     }));
 
     if ($(`.packinItem`).on(`click`, function (e) {
-        $(`.focusedPieceText h4, .focusedPieceText p, .laptopScreen img`).addClass(`invisible`);
+        $(`.focusedPieceText h4,
+        .focusedPieceText h5, 
+        .focusedPieceText p,
+        .liveButtons, 
+        .laptopScreen img`).addClass(`invisible`);
         $(`.letsGetPackin`).removeClass(`invisible`);
-        $(`.letsGetPackin`).addClass(`visible`);
+        $(`.item`).removeClass(`itemFocus`);
+        $(this).addClass(`itemFocus`);
+        $(`h4.letsGetPackin, p.letsGetPackin`).addClass(`visible`);
+        $(`button.letsGetPackin`).addClass(`visibleButton`);
     }));
 
     if ($(`.cocktailsItem`).on(`click`, function (e) {
-        $(`.focusedPieceText h4, .focusedPieceText p, .laptopScreen img`).addClass(`invisible`);
+        $(`.focusedPieceText h4,
+        .focusedPieceText h5, 
+        .focusedPieceText p,
+        .liveButtons, 
+        .laptopScreen img`).addClass(`invisible`);
+        $(`.item`).removeClass(`itemFocus`);
+        $(this).addClass(`itemFocus`);
         $(`.lastMinuteCocktails`).removeClass(`invisible`);
-        $(`.lastMinuteCocktails`).addClass(`visible`);
+        $(`h4.lastMinuteCocktails, p.letsGetPackin`).addClass(`visible`);
+        $(`button.lastMinuteCocktails`).addClass(`visibleButton`);
     }));
 }
 
